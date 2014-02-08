2b9c376c11d31e5cefa97da67c0eb31f954fd854
lepisma <abhinav.tushar.vs@gmail.com>
2014-02-08 12:56:53 +0530

    Adds actuation class

---
+++ doraemon/Actuation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doraemon
+{
+    public class Actuation
+    {
+        protected void traverse()
+        {
+            // Move the bot in forward direction
+        }
+
+        protected void turnRight()
+        {
+            // Turn the bot right
+        }
+
+        protected void turnLeft()
+        {
+            // Turn the bot left
+        }
+
+        protected void align()
+        {
+            // Aligns the bot facing back to Kinect
+        }
+
+        protected void stop()
+        {
+            // Stops the bot
+        }
+    }
+}
