276780146b98b4c3fa6ac7c79394fd8023700253
lepisma <abhinav.tushar.vs@gmail.com>
2014-02-08 11:03:42 +0530

    Initialized KinectSensor

--- doraemon/MainWindow.xaml.cs
+++ doraemon/MainWindow.xaml.cs
@@ -27,9 +27,75 @@ namespace doraemon
     /// </summary>
     public partial class MainWindow : Window
     {
+        KinectSensor sensor;
+        WriteableBitmap depthBitmap;
+        WriteableBitmap colorBitmap;
+        DepthImagePixel[] depthPixels;
+        byte[] colorPixels;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+
+                if (null == this.sensor)
+                {
+                    Console.WriteLine("No sensor found.");
+                }
+
+                if (null != this.sensor)
+                {
+                    this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                    this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                    this.sensor.SkeletonStream.Enable();
+                    this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                    this.depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+                    this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+                    this.depthBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                    this.sensor.AllFramesReady += sensor_AllFramesReady;
+
+                    try
+                    {
+                        this.sensor.Start();
+                    }
+                    catch (IOException)
+                    {
+                        this.sensor = null;
+                    }
+                }
+            }
+        }
+
+        void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+                {
+                    using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
